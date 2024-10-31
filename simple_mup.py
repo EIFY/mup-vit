fix embedding scaling

simple_mup.py
@@ -63,11 +63,13 @@ class Patchifier(uu.Conv2d):
 
 
 def learned_embeddings(num, hidden_dim):
-    weight = uu.Parameter(torch.normal(mean=0.0, std=1.0, size=(num, hidden_dim)), mup_type="weight")
-    # Empirical 1/sqrt(hidden_dim) embedding scaling law from https://arxiv.org/abs/2407.17465
-    # ViT doesn't need to look up embeddings but needs to tile/cat them, so almost-raw Parameter()
-    # is preferred over uu.Embedding().
-    return scale_bwd(weight, hidden_dim ** -0.5)
+    # Empirical embedding LR 1/sqrt(hidden_dim) from https://arxiv.org/abs/2407.17465 applied with
+    # mup_type="weight".
+    return uu.Parameter(torch.normal(mean=0.0, std=1.0, size=(num, hidden_dim)), mup_type="weight")
+
+
+def embedding_scale(embeddings, batch_size):
+    return S.scale_bwd(embeddings, batch_size ** -0.5)
 
 
 def classifier_head(hidden_dim, num_classes, representation_size):

simple_vit.py
@@ -61,6 +61,7 @@ class SimpleVisionTransformer(nn.Module):
         self.patchifier = p.Patchifier(hidden_dim=hidden_dim, patch_size=patch_size)
         self.add = p.add
         self.pool = getattr(p, pool_type)
+        self.embedding_scale = p.embedding_scale
 
         h = w = image_size // patch_size
         self.seq_length = h * w
@@ -136,11 +137,12 @@ class SimpleVisionTransformer(nn.Module):
     def forward(self, x: torch.Tensor):
         # Reshape and permute the input tensor
         x = self._process_input(x)
+        n = x.shape[0]
         if self.pos_embedding is not None:
-            x = self.add(x, self.pos_embedding)
+            x = self.add(x, self.embedding_scale(self.pos_embedding, n))
         if self.register:
-            n = x.shape[0]
-            x = torch.cat([torch.tile(self.reg, (n, 1, 1)), x], dim=1)
+            reg = self.embedding_scale(self.reg, n)
+            x = torch.cat([torch.tile(reg, (n, 1, 1)), x], dim=1)
         x = self.encoder(x)
         x = self.pool(x, self.register, self.seq_length)
         x = self.heads(x)