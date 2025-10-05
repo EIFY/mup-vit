just wd for sign and c_sq for the rest, bug fixes

scion.py
@@ -515,7 +515,10 @@ class Scion(torch.optim.Optimizer):
         for group, norm_backend, p in self.assigned_parameters():
             lr = group['lr']
             momentum = group['momentum']
-            wd = lr * (lr if group['corrected'] else group['max_lr']) * (2 - momentum) / (2 * momentum * group['c_sq'])
+            if group['corrected']:
+                wd = lr ** 2 * (2 - momentum) / (2 * momentum * group['c_sq'])
+            else:
+                wd = lr * group['weight_decay']
             g = p.grad
             if g is None:
                 continue