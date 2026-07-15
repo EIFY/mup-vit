444194c8192b22c3a066cce5b03c47dfbd420e6e
EIFY <chuanchih@gmail.com>
2026-07-14 19:10:56 -0700

    fix unnormed_driver.sh

--- scripts/unnormed_driver.sh
+++ scripts/unnormed_driver.sh
@@ -4,7 +4,7 @@ scripts=("unnormed_corrected_lr.sh" "unnormed_corrected_wd.sh" "unnormed_correct
 
 len=${#scripts[@]}
 
-python script_gen.py
+python unnormed_script_gen.py
 
 while [ ! -f done ]; do
 	for ((i=0; i<$len-1; i++)); do
