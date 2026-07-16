0ff3fdf2f4966037ee70cb649d3263659d0348b5
EIFY <chuanchih@gmail.com>
2026-07-15 19:42:09 -0700

    file "done" can also collide

--- scripts/unnormed_driver.sh
+++ scripts/unnormed_driver.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-scripts=("unnormed_corrected_lr.sh" "unnormed_corrected_wd.sh" "unnormed_corrected_sign_lr.sh" "unnormed_corrected_sign_wd.sh" "unnormed_corrected_c_sq_lr.sh" "unnormed_wd.sh" "done")
+scripts=("unnormed_corrected_lr.sh" "unnormed_corrected_wd.sh" "unnormed_corrected_sign_lr.sh" "unnormed_corrected_sign_wd.sh" "unnormed_corrected_c_sq_lr.sh" "unnormed_wd.sh" "unnormed_done")
 
 len=${#scripts[@]}
 
 python unnormed_script_gen.py
 
-while [ ! -f done ]; do
+while [ ! -f unnormed_done ]; do
 	for ((i=0; i<$len-1; i++)); do
 		curr="${scripts[i]}"
 		next="${scripts[i+1]}"
--- scripts/unnormed_script_gen.py
+++ scripts/unnormed_script_gen.py
@@ -358,8 +358,8 @@ with open(file_prefix + "wd.sh", "w") as f:
 if not final_acc:
     sys.exit()
 
-pathlib.Path('done').touch()
+pathlib.Path('unnormed_done').touch()
 print('Done!')
 
 # Files opened:
-# ['unnormed_corrected_lr.sh', 'unnormed_corrected_wd.sh', 'unnormed_corrected_sign_lr.sh', 'unnormed_corrected_sign_wd.sh', 'unnormed_corrected_c_sq_lr.sh', 'unnormed_wd.sh', 'done']
+# ['unnormed_corrected_lr.sh', 'unnormed_corrected_wd.sh', 'unnormed_corrected_sign_lr.sh', 'unnormed_corrected_sign_wd.sh', 'unnormed_corrected_c_sq_lr.sh', 'unnormed_wd.sh', 'unnormed_done']
