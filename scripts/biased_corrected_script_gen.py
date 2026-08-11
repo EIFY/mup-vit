f3eef8ed36bb748acc75c9162b4b4f44e4f9ab6f
EIFY <chuanchih@gmail.com>
2026-08-10 20:47:44 -0700

    less magic numbers

--- scripts/biased_corrected_script_gen.py
+++ scripts/biased_corrected_script_gen.py
@@ -642,8 +642,7 @@ with open(file_prefix + "power.sh", "w") as f:
     print("# Polynomial decay power tuning:", file=f)
 
     key = 'power'
-    initial_val = 1.1
-    initial_value = {key: initial_val, 'lr': default['lr']}
+    initial_value = {key: default[key], 'lr': default['lr']}
     tuner = LRPowerAutoTuner(
         factor=2**0.5, initial_value=initial_value, comp=0.5, p_tuner=PowerAutoTuner, key=key, coarse=0.3, fine=0.1, curr=default, f=f)
     power_default, final_acc = tuner.run()
